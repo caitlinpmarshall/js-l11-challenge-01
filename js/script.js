916cb05020aad3d959179e3f99572aad0f84df71
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-05-14 17:03:48 -0400

    Started button event listener

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+button.addEventListener("click", function{
+    
+});
