77b898b02c1996b1a3fde8600706b35716bddfc2
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-05-16 07:26:54 -0400

    getImage async function added

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-button.addEventListener("click", function{
-    
-});
+const getImage = async function(){
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    const images = await res.json();
+    console.log(images);
+};
+
+getImage();
